2a551d6895d31f3362e800297087735426e5f33c
Troy Sargent <troyjsargent@gmail.com>
2020-09-18 08:26:16 -0500

    search bar filters table and unfilters on backspace

--- src/App.js
+++ src/App.js
@@ -5,35 +5,42 @@ import Search from './components/Search'
 
 export default function App() {
   const [employeeState, setEmployeeState] = useState({
-    employees: []
+    employees: [],
+    filteredEmployees: []
 })
 
 useEffect(() => {
    const fetchData = async () => {
-     let {results} = await fetch('https://randomuser.me/api/?results=10', {
+     let {results} = await fetch('https://randomuser.me/api/?results=10&nat=US', {
          method: 'GET'
      }).then(response => response.json());
 
-     setEmployeeState({employees : results});
+     setEmployeeState({...employeeState, employees : results});
   }
 
   fetchData();
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, [])
 
   const searchForEmployee = e => {
+  setEmployeeState({filteredEmployees : []});
   let searchValue = e.target.value.toLowerCase();
-  let filteredEmployeeList = employeeState.employees.filter(employee => {
-    let name = `${employee.name.first} ${employee.name.last}`.toLowerCase()
-    return name.includes(searchValue)
-  })
-  setEmployeeState({employees : filteredEmployeeList});
-};
+  let searchResults = [...employeeState.employees];
+
+  searchResults = searchResults.filter(employee => {
+      let name = `${employee.name.first} ${employee.name.last}`.toLowerCase()
+      return name.includes(searchValue)
+    });
+    setEmployeeState({...employeeState, filteredEmployees : searchResults});
+
+  };
 
   return (
     <>
     <Search searchForEmployee={searchForEmployee}/>
     <Container>
-      <SimpleTable employees={employeeState.employees}/>
+      {console.log(employeeState)}
+      <SimpleTable employees={employeeState.filteredEmployees.length ? employeeState.filteredEmployees : employeeState.employees}/>
     </Container>
     </>
   );
